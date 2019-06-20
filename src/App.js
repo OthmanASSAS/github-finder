251c2829bab7f668c8964b1fe4ab50c5d85058c8
Othman Assas <othman_assas@hotmail.fr>
2019-06-20 23:46:08 +0200

    Clear users from state

--- src/App.js
+++ src/App.js
@@ -27,12 +27,16 @@ class App extends Component {
 
   // Search Github users
   searchUsers = async text => {
-    this.setState({loading:true})
+    this.setState({ loading: true })
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ users: res.data.items, loading: false });
   }
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false })
+
 
   render() {
     const { users, loading } = this.state;
@@ -41,7 +45,7 @@ class App extends Component {
 
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 } />
           <Users loading={loading} users={users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
 
 class Search extends Component {
     state = {
         text: ''
+    };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
     }
 
     onSubmit(e) {
         e.preventDefault();
         this.props.searchUsers(this.state.text);
-        this.setState({text:''})
+        this.setState({ text: '' })
     }
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value })
     render() {
+
         const { text } = this.state;
+        const { showClear,clearUsers } = this.props;
         return (
             <div>
                 <form onSubmit={this.onSubmit.bind(this)} className="form">
@@ -26,6 +36,9 @@ class Search extends Component {
                     />
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {showClear && (
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+                )}
             </div>
         );
     }
